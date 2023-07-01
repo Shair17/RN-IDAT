32579b6791ca5fd06a480ea16bfb4a5fb177b799
Shair17 <jimmy_jhair17@hotmail.com>
2023-07-01 11:32:02 -0500

    nevegacion con react navigation

--- navigation/Root.tsx
+++ navigation/Root.tsx
@@ -1,76 +1,27 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {SilabosScreen} from '../screens/SilabosScreen';
-import {SilaboScreen} from '../screens/SilaboScreen';
-import {EditSilaboScreen} from '../screens/EditSilaboScreen';
-import {Semana15} from '../screens/Semana15';
-import {OtraPantalla} from '../screens/OtraPantalla';
-import {Semana16} from '../screens/Semana16';
+import {HomeScreen} from '../screens/HomeScreen';
+import {ProfileScreen} from '../screens/ProfileScreen';
+import {SettingsScreen} from '../screens/SettingsScreen';
 
 export type StackRootParams = {
-  Semana16: undefined;
-  Semana15: undefined;
-  OtraPantalla: undefined;
-  SilabosScreen: undefined;
-  SilaboScreen: {
-    id: string;
-  };
-  EditSilaboScreen: {
-    id: string;
-  };
+  HomeScreen: undefined;
+  ProfileScreen: undefined;
+  SettingsScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<StackRootParams>();
 
-export const Root: React.FC = () => {
+export const Root = () => {
   return (
     <Stack.Navigator
-      initialRouteName="Semana16"
+      initialRouteName="HomeScreen"
       screenOptions={{
         animation: 'slide_from_right',
       }}>
-      <Stack.Screen
-        name="Semana16"
-        component={Semana16}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Semana15"
-        component={Semana15}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="OtraPantalla"
-        component={OtraPantalla}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="SilabosScreen"
-        component={SilabosScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="SilaboScreen"
-        component={SilaboScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="EditSilaboScreen"
-        component={EditSilaboScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
     </Stack.Navigator>
   );
 };
--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {StackRootParams} from '../navigation/Root';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-export const SettingsScreen = () => {
-  return null;
+interface Props
+  extends NativeStackScreenProps<StackRootParams, 'SettingsScreen'> {}
+
+export const SettingsScreen: React.FC<Props> = ({navigation}) => {
+  return (
+    <View>
+      <Text style={{fontWeight: 'bold', color: '#000', fontSize: 24}}>
+        Hola, soy la pantalla SettingsScreen
+      </Text>
+      <Button
+        title="Ir a HomeScreen"
+        onPress={() => {
+          navigation.navigate('HomeScreen');
+        }}
+      />
+    </View>
+  );
 };
